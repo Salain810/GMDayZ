f57c94238d29665eb597af199e988508826312f1
Tom Darga <tomdarga@gmail.com>
2023-11-21 13:53:07 -0500

    fixed?

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,4 +1,4 @@
 cd C:\DayZ
 git pull
-rem cd c:\dayz\servers\0\profiles\battleye\bec
-rem start Bec.exe -f Config.cfg --dsc
+cd c:\dayz\servers\0\profiles\battleye\bec
+start Bec.exe -f Config.cfg --dsc
